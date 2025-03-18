a2780c11ca9714c0b328c76c81290414710e919e
meenugamadhavi <183721591+meenugamadhavi@users.noreply.github.com>
2025-03-18 22:47:18 +0530

    feat: added view and controller classes

--- js/controller.js
+++ js/controller.js
@@ -1,2 +1,41 @@
+import { View } from "./view.js";
 
-const a = 10;
+class Todo {
+  createTask() {}
+  deleteTasks() {}
+}
+
+class Controller {
+  task;
+  view;
+  constructor(task, view) {
+    this.task = task;
+    this.view = view;
+  }
+
+  error() {
+    console.log("Invalid Task Id");
+    this.start();
+  }
+
+  ListOfOptions() {
+    return {
+      1: this.task.createTask,
+      2: this.task.updateTask,
+      3: this.task.deleteTasks,
+    };
+  }
+
+  start() {
+    const userChoice = +prompt("Enter Your Choice");
+    const tasks = this.ListOfOptions();
+    console.log(tasks);
+    if (!(userChoice in tasks)) this.error();
+  }
+}
+
+const todo = new Todo();
+const view = new View();
+
+const task = new Controller(todo, view);
+task.start();
